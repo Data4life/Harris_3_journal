237fc702cd3cb2b0994ef2390e4a6f50fea81395
Wyland Harris <wyland.harris@gmail.com>
2016-01-21 13:22:26 -0600

    NetBeans Slick Example

---
+++ Harris_3_SlickExample/src/slickexample/Blocked.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package slickexample;
+
+/**
+ *
+ * @author Data4Life
+ */
+public class Blocked {
+
+
+	public static boolean[][] Blocked;
+
+	public static boolean[][] getblocked() {
+
+		return Blocked;
+
+	}
+
+};
+
